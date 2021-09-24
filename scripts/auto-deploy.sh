6897fd54572a5712e8e7f029f98ba960ab23ec44
Kevin Gibbons <bakkot@gmail.com>
2021-09-23 20:16:53 -0700

    Meta: Keep old years in gh-pages (#2532)

--- scripts/auto-deploy.sh
+++ scripts/auto-deploy.sh
@@ -8,12 +8,14 @@ declare -r GH_USER_NAME="Bot"
 declare -r COMMIT_MESSAGE="Update gh-pages"
 
 
-cd "$(dirname "$BASH_SOURCE")"/../out
+cd "$(dirname "$BASH_SOURCE")"/..
+git clone --depth 1 --branch gh-pages "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" gh-pages
+find gh-pages -type f \! \( -path 'gh-pages/2*' -o -path 'gh-pages/.git*' \) -exec rm -rf {} \;
+cp -r out/* gh-pages
 
-git config --global user.email "${GH_USER_EMAIL}"
-git config --global user.name "${GH_USER_NAME}"
-git config --global init.defaultBranch gh-pages
-git init
+cd gh-pages
+git config user.email "${GH_USER_EMAIL}"
+git config user.name "${GH_USER_NAME}"
 git add -A
-git commit --message "${COMMIT_MESSAGE}"
-git push --force "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" gh-pages
+git commit --allow-empty --message "${COMMIT_MESSAGE}"
+git push origin gh-pages
