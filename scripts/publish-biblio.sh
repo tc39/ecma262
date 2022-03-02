f1818008b96f056e66930e5f2b1599a91d425c2a
Kevin Gibbons <bakkot@gmail.com>
2022-03-02 15:58:07 -0800

    Meta: publish biblio to npm (#2393)

---
+++ scripts/publish-biblio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+
+$(npm bin)/ecmarkup --verbose spec.html --write-biblio biblio/biblio.json /dev/null
+
+cp LICENSE.md biblio/
+
+cd biblio
+
+COMMIT_COUNT=$(git rev-list --count HEAD)
+npm version --no-git-tag-version "1.0.${COMMIT_COUNT}"
+
+SHORT_COMMIT=$(git rev-parse --short HEAD)
+LONG_COMMIT=$(git rev-parse --verify HEAD)
+echo "
+This version was built from commit [${SHORT_COMMIT}](https://github.com/tc39/ecma262/tree/${LONG_COMMIT})." >> README.md
+
+npm publish --access public
