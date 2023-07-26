a524f33e208e565d0a38b0d63d339fdacf71b751
Jordan Harband <ljharb@gmail.com>
2023-07-26 10:50:43 -0700

    Meta: fail IPR check on commits with unknown authors (#3128)
     - also, compare everything in lowercase

--- scripts/check-form.js
+++ scripts/check-form.js
@@ -76,18 +76,62 @@ const initial = '11db17a21add028b8d12930a8b047af1df2d3194'; // git rev-list --ma
 
 const perPage = 100;
 
+const legacyCommitsWithUnknownAuthors = new Set([
+	'6e2f689b1bcecaf59f5e7c76e544443ffe4ed299',
+	'a103b287cd19bdc51c7a3d8d7c1431b1506a74e2',
+	'2527be4344c78e5ed56aef8e08463160f5bb2154',
+	'5c9f191c9e6ccfc909817510bf74fd64ea145ac4',
+	'8710d2b2a8906d0d41e90cfcb9f47573d8d573fb',
+	'0b89915d3ccbf372e8114d823688d74e7778bfdb',
+	'4374762005846b779d1cc4f03aeababe41af0e79',
+	'676dae0955b072a31b05eafdab17dfcd8f7b4975',
+	'2724be0f9aeb6265c60935334a3bdc293b2c4b4e',
+	'1c6fb24ef7b29558593ce6fd4310c3bc4fe67d82',
+	'dcc9584e642b55b8a99df91f2e80d065e3d2451c',
+	'daef7b05b2625caf06ab546acd6eb654e9777d16',
+	'eb0615ea028000d84f3d5dcd9d94a74558d3a206',
+	'50ec9b4195df6caf4da2352e0edb19024b515f2d',
+	'8d7995d6f7538534f2a54af4811c939ac8614676',
+	'1046a423adf3ea9c5460cdbdf3a9c156f52f1632',
+	'cdf5865b7da09cce276581f134349e24d2b6c199',
+	'b7d01876c19751e4c75d9910ce4fb2e0d1dc45fa',
+	'cc46cc38b9374d13f958cc6992daaec73d5fbecd',
+]);
+
+function getAuthorFromCommit(commitObj) {
+	if (!commitObj) {
+		return false;
+	}
+	const { author } = commitObj;
+	if (!author) {
+		return Symbol(commitObj.sha);
+	}
+	return author.login;
+}
+
 async function getAllCommits(page = 1) {
 	const commitsURL = `https://api.github.com/repos/${slug}/commits?anon=1&per_page=${perPage}&page=${page}&sha=${sha}`;
 	const commits = await request(commitsURL).then((json) => JSON.parse(json));
 	return [...new Set([].concat(
-		commits.flatMap(x => x?.author?.login || []),
+		commits.flatMap(x => getAuthorFromCommit(x) || []),
 		commits.length < perPage ? [] : await getAllCommits(page + 1),
 	))];
 }
 
 const authors = getAllCommits().then((authors) => {
-	console.log(`Found ${authors.length} authors: ${authors.join(',')}\n`);
-	return authors;
+	const knowns = authors.filter(x => typeof x === 'string');
+	const unknowns = authors.filter(x => typeof x === 'symbol' && !legacyCommitsWithUnknownAuthors.has(x.description));
+	console.log(`Found ${knowns.length} authors: ${knowns.join(',')}\n`);
+	if (unknowns.length > 0) {
+		console.log(`${unknowns.length} commits have an unknown author: ${unknowns.map(x => x.description).join(',')}\n`);
+	}
+	if (legacyCommitsWithUnknownAuthors.size > 0) {
+		console.log(`${legacyCommitsWithUnknownAuthors.size} commits with an unknown author are legacy: ${[...legacyCommitsWithUnknownAuthors].join(',')}\n`);
+	}
+	if (unknowns.length > 0) {
+		throw unknowns;
+	}
+	return knowns;
 });
 
 const teamURL = (team) => `https://api.github.com/orgs/tc39/teams/${team}`;
@@ -107,12 +151,12 @@ function getMembers(teamID, page = 1) {
 
 const aliases = new Map([
 	['bmeck', 'bfarias-godaddy'],
-	['PeterJensen', 'P-Jensen'],
+	['PeterJensen', 'P-Jensen'].map(x => x.toLowerCase()),
 ]);
 
 function handler(kind) {
 	return (data) => {
-		const names = new Set(data.map(x => x.login));
+		const names = new Set(data.map(x => x.login.toLowerCase()));
 		aliases.forEach((alias, main) => {
 			if (names.has(main)) {
 				names.add(alias);
@@ -138,9 +182,9 @@ const usernames = request(sheetData).then((json) => JSON.parse(json)).then(data 
 	const usernames = new Set(
 		data.values
 			.flat(1)
-			.map(x => x.replace(/^(https?:\/\/)?github\.com\//, '').replace(/^@/, ''))
+			.map(x => x.replace(/^(https?:\/\/)?github\.com\//, '').replace(/^@/, '').toLowerCase())
 			.filter(x => /^[a-z0-9_-]{1,39}$/gi.test(x))
-			.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+			.sort((a, b) => a.localeCompare(b))
 	);
 	console.log(`Found ${usernames.size} usernames: ` + [...usernames].join(',') + '\n');
 	return usernames;
@@ -156,7 +200,7 @@ const exceptions = new Set([
 	'GeorgNeis',
 	'natashenka', // Google employee
 	'IgorMinar', // former Google employee
-]);
+].map(x => x.toLowerCase()));
 
 // TODO: remove these as they sign the form
 const legacy = new Set([
@@ -166,17 +210,14 @@ const legacy = new Set([
 	'ivan-pan',
 	'szuend',
 	'chrikrah',
-	'daemon1024',
 	'viktmv',
 	'bathos',
 	'johnnyrainbow',
 	'Kriyszig',
 	'Tomy8s',
-	'targos',
 	'ahungry',
 	'divmain',
 	'RReverser',
-	'charmander',
 	'him2him2',
 	'jungshik',
 	'tmerr',
@@ -193,12 +234,12 @@ const legacy = new Set([
 	'prayagverma',
 	'UltCombo',
 	'ReadmeCritic',
-	'DavidBruant',
 	'dslomov',
 	'DmitrySoshnikov',
 	'jsreeram',
 	'antony-jeong',
-]);
+	'bojavou',
+].map(x => x.toLowerCase()));
 
 Promise.all([usernames, authors, delegates, emeriti]).then(([usernames, authors, delegates, emeriti]) => {
 	let legacyCount = legacy.size;
