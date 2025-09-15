6a37f2b9bb736115b4ae0826bc4532909fbab4f9
Kevin Gibbons <bakkot@gmail.com>
2025-09-15 13:23:10 -0600

    Meta: fix PR previews (#3679)

--- scripts/auto-deploy.sh
+++ scripts/auto-deploy.sh
@@ -2,16 +2,27 @@
 
 set -euxo pipefail
 
+DEST_DIR="gh-pages"
+COMMIT_MESSAGE="Update gh-pages"
+if [[ "${1:-}" == "--pr" && -n "${2:-}" ]]; then
+  DEST_DIR="gh-pages/pr/$2"
+  COMMIT_MESSAGE="Update gh-pages for PR $2"
+fi
 
 declare -r GH_USER_EMAIL="bot@tc39"
 declare -r GH_USER_NAME="Bot"
-declare -r COMMIT_MESSAGE="Update gh-pages"
+declare -r COMMIT_MESSAGE
 
 
 cd "$(dirname "$BASH_SOURCE")"/..
 git clone --depth 1 --branch gh-pages "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" gh-pages
-find gh-pages -type f \! \( -path 'gh-pages/2*' -o -path 'gh-pages/.git*' \) -exec rm -rf {} \;
-cp -r out/* gh-pages
+if [[ "${1:-}" == "--pr" ]]; then
+  rm -rf "$DEST_DIR"
+else
+  find gh-pages \( -path 'gh-pages/.git' -o -path 'gh-pages/pr' -o -path 'gh-pages/2*' \) -prune -o -type f -exec rm -f {} +
+fi
+mkdir -p "$DEST_DIR"
+cp -r out/* "$DEST_DIR"
 
 cd gh-pages
 git config user.email "${GH_USER_EMAIL}"
